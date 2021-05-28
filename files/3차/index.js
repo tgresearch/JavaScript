4c561997be9ab5df353c6dee2bcbc526a02ae019
tgresearch <49558813+tgresearch@users.noreply.github.com>
2021-05-28 16:57:46 +0900

    .
    complete video 2

--- files/3차/index.js
+++ files/3차/index.js
@@ -1,4 +1,6 @@
 ###DATATYPES
+/** Video #1 **/
+
 ##OBJECTS
 //you can name this values but like the next expression
 const jorgeInfo =  ["Kim","Park","29", true,"Seoul"];
@@ -32,5 +34,52 @@ console.log(jorgeInfo);
 
 //Do not forget coma ',' in the right place
 //remember read errors on google chrome console
+
+/** Video #2 **/
+
+const jorgeInfo = {
+  name: "Jorge",
+  age: 27,
+  gender:"Male"
+}
+console.log(jorgeInfo.gender);
+// display 2 objects -> console is also object, is a built in function
+console.log(jorgeInfo, console);
+//how to put code that calls and say hello
+console.log("Hello");
+console.log("Hello"+" "+"Park");
+
+##FUNCTIONS
+//eficient way to use we make a function
+//declare a function
+function sayHello(){
+  console.log("Hello");
+}
+//call function
+sayHello();
+//hello to friends
+
+function sayHello(potatoe){
+  console.log("Hello", potatoe);
+}
+
+sayHello("Jorge");
+console.log("HI");
+//lets add
+sayHello("Jorge",15);
+//but it does not add it cause function is not prepared for another variable number
+//lets add another 
+
+function sayHello(name, age){
+  console.log('Hello!', name, " you are ", age, " years old.");
+}
+
+sayHello("Jorge", 21);
+
+
+
+
+
+
 
 
