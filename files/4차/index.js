1b74959bcb7b7a05951e617a4fa4282df649f9e5
tgresearch <49558813+tgresearch@users.noreply.github.com>
2021-05-28 17:33:14 +0900

    .
    video #1

--- files/4차/index.js
+++ files/4차/index.js
@@ -1,1 +1,21 @@
+/** Video #1 **/
+//check
+console.log(document);
+//gives html document via JS
+
+const title = document.getElementById("title");
+
+console.log(title);
+//then we want to change something
+
+//DOM - document object model
+//take elements and turn them into objects
+
+title.innerHTML = "Hi!";
+
+//then you change the html
+// originally it said "Jorge" but replaced by "Hi!"
+//you can change how DOM look like
+
+/** Video #2 **/
 
