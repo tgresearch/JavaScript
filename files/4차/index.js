3979a847457dd4718048607dea703b046b1451ba
tgresearch <49558813+tgresearch@users.noreply.github.com>
2021-05-28 17:45:37 +0900

    .
    video2

--- files/4차/index.js
+++ files/4차/index.js
@@ -18,4 +18,31 @@ title.innerHTML = "Hi!";
 //you can change how DOM look like
 
 /** Video #2 **/
+//Use `` , '' and ""
+function sayHello(name, yts){
+  console.log(`Hello ${name} you are ${yts} away from success`);
+}
+
+sayHello("Jorge",25);
+
+//
+const greetJorge = sayHello("Jorge", yts);
+
+console.log(greetJorge);
+
+//
+const calculator = {
+  plus: function(a,b){
+    return a+b;
+  },
+  substract: function(){},
+  multiply: function(){},
+  power: function(){},
+}
+
+//
+const plus = calculator.plus(5,5);
+console.log(plus)
+
+
 
