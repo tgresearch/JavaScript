c5e0999a5ae7a5973034cbc820817d8cd18a1988
tgresearch <49558813+tgresearch@users.noreply.github.com>
2021-05-28 16:31:49 +0900

    .
    1st video

--- files/3차/index.js
+++ files/3차/index.js
@@ -1,1 +1,36 @@
+###DATATYPES
+##OBJECTS
+//you can name this values but like the next expression
+const jorgeInfo =  ["Kim","Park","29", true,"Seoul"];
+//top declare and object you use "{}"
+const jorgeInfo = {
+  name: "Jorge",
+  age: 27,
+  gender:"Male"
+}
+//access info with "." like ...
+console.log(jorgeInfo.gender);
+//change info inside object with ...
+jorgeInfo.gender="Non-show";
+console.log(jorgeInfo);
+//Combine array and object
+const jorgeInfo = {
+  favMovies: ["Ready Player One", "The Matrix", "Never Ending Story"],
+  favFood:[
+    {
+      name:"Burger", 
+      fatcont:true
+    },
+    {
+      name:"Pizza",
+      fatcont:true
+    }
+  ]
+}
+console.log(jorgeInfo);
+// object inside of array inde and object
+
+//Do not forget coma ',' in the right place
+//remember read errors on google chrome console
+
 
