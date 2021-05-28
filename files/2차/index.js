a6f9713bc0c71fe595d5b9cea27c6c1f8566af3d
tgresearch <49558813+tgresearch@users.noreply.github.com>
2021-05-28 15:58:46 +0900

    .
    finish video#1

--- files/2차/index.js
+++ files/2차/index.js
@@ -1,1 +1,34 @@
+/** Video #1
+**/
+
+//comments - are notes you can leave
+//sometimes you want to leave a note to request for HELP, TODO, etc.
+
+##MULTILINE COMMENT
+/*
+to do : finish
+to do : finish
+to do : finish
+*/
+
+//first thing to save in a variable is a STRING
+##STRING
+const what = "Nicolas";
+// wihtout "" -> try to find the variable
+console.log(what);
+//you can save text and emojis
+const what = "565656";
+// here the number 565656 are text not numbers
+##BOOLEAN
+const what = false;
+//or
+const what = true;
+##NUMBER
+const what = 1313;
+##FLOAT
+const what = 12.1;
+//a number with a floating number
+
+/** Video #2
+**/
 
