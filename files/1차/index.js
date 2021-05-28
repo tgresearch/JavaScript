0edc0145b167817af7439494fe441390fa4d5143
tgresearch <49558813+tgresearch@users.noreply.github.com>
2021-05-28 15:40:03 +0900

    .
    first video done

---
+++ files/1차/index.js
@@ -0,0 +1,16 @@
+/** Video #1
+**/
+###Grammar
+##Let
+##variables
+let a=221;
+let b=a-5;
+a = 4; //update a
+//execute code from top to bottom
+console.log(b);
+console.log(b,a);
+
+/** Video #2
+**/
+##constants
+
