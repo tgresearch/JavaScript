ddf0749406255cd12681ff8e9273a6d1526e29c6
tgresearch <49558813+tgresearch@users.noreply.github.com>
2021-06-04 14:20:26 +0900

    video 1 finished
    .

--- files/7차/index.js
+++ files/7차/index.js
@@ -1,2 +1,56 @@
 /* Video #1 */
+##1
+
+const title = document.querySelector("#title");
+
+const clicked_class = "clicked";
+
+function handleClick(){
+  const currentClass = title.className;
+  //console.log(currentClass);
+  if(currentClass !== clicked_class){
+    title.className = clicked_class;
+  } else {
+    title.className = "";
+  }
+}
+
+function init(){
+  title.addEventListener("click",handleClick);
+}
+init();
+
+/// but then we realize that the class is not letting other class and functions work
+/// go to DMN again to check references
+///try to find a <div>
+///https://developer.mozilla.org/en-US/docs/Web/API/HTMLDivElement
+///then check html propersties to find "classNAme"
+/// https://developer.mozilla.org/en-US/docs/Web/API/Element/className
+/// then we realize we want to use "classList" isntead because it has methods
+/// https://developer.mozilla.org/en-US/docs/Web/API/Element/classList
+/// because we need to check for more than one class then we also do "contains"
+/// constains check if the value exist
+
+##2
+
+const title = document.querySelector("#title");
+
+const clicked_class = "clicked";
+
+function handleClick(){
+//  const hasClass = title.classList.contains(clicked_class);
+//  if(!hasClass){
+//    title.classList.add(clicked_class);
+//  } else {
+//    title.classList.remove(clicked_class);
+//  }
+  /// we are going to use toggle instead to encapsulate all we previously did here
+  title.classList.toggle(clicked_class);
+}
+
+function init(){
+  title.addEventListener("click",handleClick);
+}
+init();
+
 /* Video #2 */
