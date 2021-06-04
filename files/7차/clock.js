9ed5d11ff8bf179e0a5a6199e36c5848ce3a5fe1
tgresearch <49558813+tgresearch@users.noreply.github.com>
2021-06-04 14:49:07 +0900

    finish video #2
    .

--- files/7차/clock.js
+++ files/7차/clock.js
@@ -1,1 +1,24 @@
+const clockContainer = document.querySelector(".js-clock"),
+      clockTitle = clockContainer.querySelector("h1");
 
+function getTime(){
+  const date = new Date();
+  const minutes = date.getMinutes();
+  const hours = date.getHours();
+  const seconds = date.seconds();
+  clockTitle.innerText = `${hours}:${minutes}:${seconds}`
+}
+
+function init(){
+  
+}
+
+init();
+
+// in the making of getTime()
+//in console you 
+// const date = new Date()
+// date
+// date.getDate()
+// date.getHours()
+// date.getMinutes()
